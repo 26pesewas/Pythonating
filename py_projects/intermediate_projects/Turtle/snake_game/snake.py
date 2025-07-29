b3ee04c0b3e33c640e694cfba372d12e8404d4e5
26pesewas <naaakuokor@gmail.com>
2025-07-29 02:53:54 +0000

    added keypress functionalities to snake

--- py_projects/intermediate_projects/Turtle/snake_game/snake.py
+++ py_projects/intermediate_projects/Turtle/snake_game/snake.py
@@ -3,14 +3,20 @@ import time
 
 screen = Screen()
 
-#global vars
-x_positions = [-10, -20, -30]
+#global constants
+X_POSITIONS = [-10, -20, -30]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
 
 class Snake():
     """Creating a snake class"""
     def __init__(self):
         self.my_snakes = []
         self.create_snake()
+        self.head = self.my_snakes[0]
 
 
     def create_snake(self):
@@ -18,16 +24,27 @@ class Snake():
             new_turtle = Turtle(shape="square")
             new_turtle.color("white")
             new_turtle.penup()
-            new_turtle.goto(x=x_positions[index], y=0)
+            new_turtle.goto(x=X_POSITIONS[index], y=0)
             self.my_snakes.append(new_turtle)
 
     def move(self):
-        game_is_on = True
-        while game_is_on:
-            screen.update()
-            time.sleep(0.1)
-            for coordinates in range(len(self.my_snakes) - 1, 0, -1):
-                new_x = self.my_snakes[coordinates - 1].xcor()
-                new_y = self.my_snakes[coordinates - 1].ycor()
-                self.my_snakes[coordinates].goto(new_x, new_y)
-            self.my_snakes[0].forward(20)
+        for coordinates in range(len(self.my_snakes) - 1, 0, -1):
+            new_x = self.my_snakes[coordinates - 1].xcor()
+            new_y = self.my_snakes[coordinates - 1].ycor()
+            self.my_snakes[coordinates].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+
